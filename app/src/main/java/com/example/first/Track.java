b66b49f36ca0edb3987dfbbc9d052d69d5c554f2
omprakashphd@outlook.com <omprakashphd@outlook.com>
2020-12-14 12:02:17 +0530

    first

---
+++ app/src/main/java/com/example/first/Track.java
@@ -0,0 +1,12 @@
+package com.example.first;
+
+public class Track {
+    public String name;
+    public String instructor;
+    Track(String name, String instructor){
+        this.name=name;
+        this.instructor=instructor;
+    }
+
+}
+
